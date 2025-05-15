01941292c7d633ac5965364721b838c004be86a7
dckyx <hubertposs@wp.pl>
2025-05-15 19:37:59 +0200

    Init

---
+++ Bookmaker_app/forms.py
@@ -0,0 +1,19 @@
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username')
+
+
+class CustomLogin(AuthenticationForm):
+    username = forms.CharField(label='Nazwa uzytkownik',max_length=50)
+    password = forms.CharField(label='Haslo',widget=forms.PasswordInput)
+
+from django import forms
+
+class KwotaForm(forms.Form):
+    kwota = forms.DecimalField(label="Kwota (zł)", min_value=0.01, max_digits=10, decimal_places=2)
