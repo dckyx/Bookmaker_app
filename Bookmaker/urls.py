7433f2a8fc1b17653d6809b6b8f08e73179adeaf
mejke <m.podlaszczyk00@gmail.com>
2025-05-19 12:28:09 +0200

    admin panel upgrade

--- Bookmaker/urls.py
+++ Bookmaker/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Bookmaker_app import views
 
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('logout/', views.logout_view, name='logout'),
--- Bookmaker_app/admin.py
+++ Bookmaker_app/admin.py
@@ -7,13 +7,37 @@ from .models import *
 
 
 
-admin.site.register(CustomUser)
 admin.site.register(ZakladyUzytkownika)
 admin.site.register(HistoriaTransakcji)
 admin.site.register(Kategoria)
-admin.site.register(Dyscyplina)
-admin.site.register(Drużyna)
-admin.site.register(Event)
 admin.site.register(Stream)
 
 # Register your models here.
+
+@admin.register(Drużyna)
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dyscyplina')  # <-- dodana kolumna
+    list_filter = ('dyscyplina',)           # <-- filtr z boku
+    search_fields = ('name',)              # <-- wyszukiwarka
+    ordering = ('dyscyplina', 'name')      # <-- sortowanie
+
+@admin.register(Dyscyplina)
+class DyscyplinaAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'saldo')
+    list_filter = ('is_staff', 'is_active', 'saldo')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username', 'email', 'first_name', 'last_name')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'datetime', 'dyscyplina','druzyna1','druzyna2','status','wynik_druzyna1','wynik_druzyna2')
+    list_filter = ('name','datetime','status')
+    search_fields = ('name','datetime','dyscyplina','druzyna1','druzyna2','status')
+    ordering = ('datetime','status')
