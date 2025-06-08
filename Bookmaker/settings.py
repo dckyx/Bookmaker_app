e39b41c59ec3f22a89867d36d1dc4cd86e4f6c3d
dckyx <hubertposs@wp.pl>
2025-06-08 17:00:00 +0200

    heroku

--- Bookmaker/settings.py
+++ Bookmaker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://bookmaker-project-48b16b55ee1c.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 AUTH_USER_MODEL = 'Bookmaker_app.CustomUser'
 
