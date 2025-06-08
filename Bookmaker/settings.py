3c5a76f68e41eba2b8c3c272e0477ce9654f42f5
dckyx <hubertposs@wp.pl>
2025-06-08 16:56:46 +0200

    Fix: different STATIC_URL and MEDIA_URL for Heroku

--- Bookmaker/settings.py
+++ Bookmaker/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
-MEDIA_URL = "static/"
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
