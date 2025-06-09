3fd1424c3b78326a0328c1c80219fb842046f4e3
dckyx <hubertposs@wp.pl>
2025-06-09 17:54:58 +0200

    heroku

--- Bookmaker/settings.py
+++ Bookmaker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'Bookmaker_app.CustomUser'
 
