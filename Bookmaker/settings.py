96aad6ffb9f6a3dc38cdf16ce0cd4afb5adf24a5
dckyx <hubertposs@wp.pl>
2025-06-13 12:53:46 +0200

    Poprawka settings.py

--- Bookmaker/settings.py
+++ Bookmaker/settings.py
@@ -200,7 +200,7 @@ TEMPLATES[0]['DIRS'] = [
     BASE_DIR / "bookmaker_app" / "templates",
     BASE_DIR / "frontend" / "build"
 ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Bookmaker_app/static')]
+STATICFILES_DIRS = [ BASE_DIR / "bookmaker-project" / "Bookmaker_app" / "static" ]
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'frontend' / 'build' / 'static',
